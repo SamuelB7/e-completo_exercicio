52b417c85e2ae22005a2cd49201202d9a2f3c838
Samuel Belo <belo.samuel@gmail.com>
2022-03-06 23:42:33 -0300

    request test

--- app/Http/Controllers/PedidosPagamentosController.php
+++ app/Http/Controllers/PedidosPagamentosController.php
@@ -3,13 +3,52 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pedidos;
+use App\Models\PedidosPagamentos;
 use Illuminate\Http\Request;
 
 class PedidosPagamentosController extends Controller
 {
     public function processPayment($purchaseId) {
-        $purchase = Pedidos::find($purchaseId);
+        $pedido = Pedidos::find($purchaseId);
+        $pedidoPagamento = PedidosPagamentos::where('id_pedido', $pedido->id)->get();
 
-        return response()->json($purchase->pedidoSituacao);
+        $external_order_id = $pedido->id;
+        $amount = $pedido->valor_total + $pedido->valor_frete;
+        $card_number = $pedidoPagamento->num_cartao;
+        $card_cvv = $pedidoPagamento->codigo_verificacao;
+        $card_expiration_date = $pedidoPagamento->vencimento;
+        $card_holder_name = $pedidoPagamento->nome_portador;
+
+        $type = '';
+        $documentType = '';
+
+        if(strlen($pedido->clientes->cpf_cnpj) == 14) {
+            $type = 'corporation';
+            $documentType = 'cnpj';
+        } else {
+            $type = 'individual';
+            $documentType = 'cpf';
+        }
+
+        $email = $pedido->clientes->email;
+
+        $customer = [
+            'external_id' => $pedido->clientes->id,
+            'name' => $pedido->clientes->nome,
+            'type' => $type,
+            'email' => $email,
+            'documents' => [
+                'type' => $documentType,
+                'number' => 
+            ]
+        ];
+
+        $data = [
+            'pedido' => $pedido,
+            'pedidoPagamento' => $pedidoPagamento,
+            'cliente' => $pedido->clientes,
+            'situação' => $pedido->pedidoSituacao
+        ];
+        return response()->json($data);
     }
 }
